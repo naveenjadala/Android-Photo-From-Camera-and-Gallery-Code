32a650800e26ddeb89aa49cea1d0750ed21d857a
naveen <naveen.jadala@gmail.com>
2020-04-11 10:45:00 +0530

    first commit

---
+++ app/src/main/java/com/example/androidcamgallery/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidcamgallery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
